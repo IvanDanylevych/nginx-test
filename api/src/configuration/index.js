a9157edd3276b4a35946722ff623fb13d88ee113
IvanDanylevych <iv.danylevych@gmail.com>
2024-12-07 17:56:01 +0200

    1123123

--- api/src/configuration/index.js
+++ api/src/configuration/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT
-const host = process.env.HOST
-const mongoURL = process.env.MONGO_URL
-module.exports = {port, host, mongoURL}
+const port = process.env.PORT ?? 3000;
+const host = process.env.HOST ?? "localhost";
+const mongoURL = process.env.MONGO_URL ?? "mongodb://api_db:27017/api";
+module.exports = { port, host, mongoURL };
