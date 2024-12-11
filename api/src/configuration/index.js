572cbd94de0243dc18003face0cd0d0482aa7949
IvanDanylevych <iv.danylevych@gmail.com>
2024-12-11 12:46:23 +0200

    update port and request

--- api/src/configuration/index.js
+++ api/src/configuration/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3001;
 const host = process.env.HOST ?? "localhost";
 const mongoURL = process.env.MONGO_URL ?? "mongodb://api_db:27017/api";
 module.exports = { port, host, mongoURL };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await fetch("/api/users");
+      return users.json();
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Данилевич Іван ІПЗм-24-2</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save t123123
+          Edit <code>src/App.js</code> and save
         </p>
         <a
           className="App-link"
